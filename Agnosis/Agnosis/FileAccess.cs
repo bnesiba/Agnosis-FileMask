4e2b07e14b98d422662dc82b9a26b7bab1b3d7c2
Brandon Nesiba <bnesiba@gmail.com>
2019-12-06 15:19:26 -0600

    Copies and masks files. does not generate spreadsheet yet.

---
+++ Agnosis/Agnosis/FileAccess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Agnosis
+{
+    public static class FileAccess
+    {
+        public static List<string> GetFilePathsInDirectory(string path)
+        {
+            try
+            {
+                return Directory.GetFiles(path, "*", SearchOption.AllDirectories).ToList();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred while getting file paths");
+                throw;
+            }
+        }
+
+        public static Dictionary<string, string> CopyFilesAndMask(List<string> filePaths,string inputDirectory, string outputDirectory)
+        {
+            Dictionary<string,string> fileNameMasks = new Dictionary<string, string>();
+            try
+            {
+                foreach (string filePath in filePaths)
+                {
+                    //get filename
+                    int fileNameStart = filePath.LastIndexOf('\\');
+                    int fileNameEnd = filePath.LastIndexOf('.');
+                    string fileName = filePath.Substring(fileNameStart + 1, fileNameEnd - fileNameStart - 1);
+                    Guid fileGuid = Guid.NewGuid();
+                    fileNameMasks.Add(fileGuid.ToString(), fileName);
+
+                    //replace filename with guid
+                    string newFilePath = filePath.Remove(fileNameStart + 1, fileNameEnd - fileNameStart - 1);
+                    newFilePath = newFilePath.Insert(fileNameStart + 1, fileGuid.ToString());
+
+                    //replace "input" with outputdir
+                    int inputDirStart = newFilePath.IndexOf(inputDirectory,StringComparison.InvariantCulture);
+                    newFilePath = newFilePath.Remove(inputDirStart, inputDirectory.Length);
+                    newFilePath =  newFilePath.Insert(inputDirStart, outputDirectory);
+                    
+                    //write file to new path;
+                    CopyFileToNewLocation(filePath,newFilePath);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred while copying files.");
+                throw;
+            }
+
+            return fileNameMasks;
+        }
+
+        private static void CopyFileToNewLocation(string originalPath, string outputPath)
+        {
+            int fileNameStart = outputPath.LastIndexOf("\\", StringComparison.InvariantCulture);
+            Directory.CreateDirectory(outputPath.Substring(0,fileNameStart+1));
+            File.Copy(originalPath, outputPath);
+        }
+    }
+}
--- Agnosis/Agnosis/Program.cs
+++ Agnosis/Agnosis/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,36 @@ namespace Agnosis
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Dictionary<string, string> fileMasks = new Dictionary<string, string>();
+                List<string> filePaths = new List<string>();
+
+                Console.WriteLine("Getting file paths...");
+                filePaths = FileAccess.GetFilePathsInDirectory("Input");
+                foreach (var path in filePaths)
+                {
+                    Console.WriteLine(path);
+                }
+
+                fileMasks = FileAccess.CopyFilesAndMask(filePaths,"Input","Output");
+
+                foreach (var file in fileMasks.Keys)
+                {
+                    Console.WriteLine($"Key: {file} - Value: {fileMasks[file]}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Console.WriteLine("An error occurred while processing files. Some files may not have been copied. Please let Brandon know about this...",e);
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
         }
     }
 }
